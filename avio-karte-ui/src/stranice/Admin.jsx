509b3516443cece944ad6a38edb6266b672ffa6c
JANA_JEVTIC <jevticjana7@gmail.com>
2024-06-14 20:52:09 +0200

    admin

--- avio-karte-ui/src/stranice/Admin.jsx
+++ avio-karte-ui/src/stranice/Admin.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import Naslov from "../komponente/Naslov";
+import {Chart} from "react-google-charts";
+import axiosZahtev from "../axiosZahtev";
 
 const Admin = () => {
+    const [poruka, setPoruka] = React.useState("");
+    const [data, setData] = React.useState([]);
+    const [options, setOptions] = React.useState({
+        title: 'Broj polazaka po aerodromima',
+        is3D: true
+    });
+
+    useEffect(() => {
+        axiosZahtev.get("/grafik")
+            .then(res => {
+                let podaci = res.data.podaci;
+                let data = [["Aerodrom", "Broj polazaka"]];
+                podaci.forEach(p => {
+                    data.push([p.naziv, parseInt(p.broj_letova)]);
+                });
+                setData(data);
+            }).catch(err => {
+            setPoruka("Greska pri ucitavanju podataka");
+        })
+    }, []);
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <Naslov naslov="Admin panel" poruka={poruka} />
+
+            <div className="mt-3">
+                <Chart
+                    chartType="PieChart"
+                    data={data}
+                    options={options}
+                    width={"100%"}
+                    height={"400px"}
+                />
+            </div>
+        </>
     );
 };
 
