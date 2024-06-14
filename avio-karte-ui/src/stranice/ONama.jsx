f7add80c267c0b1d390043e25b7e4726bf1f9d63
JANA_JEVTIC <jevticjana7@gmail.com>
2024-06-14 21:19:47 +0200

    o nama api

--- avio-karte-ui/src/stranice/ONama.jsx
+++ avio-karte-ui/src/stranice/ONama.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Naslov from "../komponente/Naslov";
 import {Col, Row} from "react-bootstrap";
 import Kartica from "../komponente/Kartica";
+import axiosZahtev from "../axiosZahtev";
 
 const ONama = () => {
 
@@ -20,6 +21,20 @@ const ONama = () => {
         }
     ];
 
+    const [user, setUser] = React.useState(null);
+
+    React.useEffect(() => {
+        //https://randomuser.me/api/
+
+        axiosZahtev.get("https://randomuser.me/api/")
+            .then(res => {
+                console.log(res.data.results[0]);
+                setUser(res.data.results[0]);
+            }).catch(err => {
+            console.log(err);
+        })
+    }, []);
+
     return (
         <>
             <Naslov naslov="O nama" poruka="" />
@@ -33,6 +48,22 @@ const ONama = () => {
                             </Col>
                         ))
                     }
+                </Row>
+
+                <Row className="m-3">
+                    <Col>
+                        <p className="mt-3">
+                            Kapetan meseca - 2024 godine je bio kapetan Marko Markovic. Marko je najbolji kapetan u istoriji avio kompanije.
+                            Marko je zavrsio fakultet za pilote i od tada je uvek bio najbolji u svom poslu.
+                        </p>
+                    </Col>
+                    <Col>
+                        {
+                            user && (
+                                <img width={500} height={600} src={user.picture.large} alt="random user" className="img img-fluid"/>
+                            )
+                        }
+                    </Col>
                 </Row>
             </div>
         </>
