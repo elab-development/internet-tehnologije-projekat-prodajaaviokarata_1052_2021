98077550e04dfb77341e8dc9ecf2ba1d7a1e0019
JANA_JEVTIC <jevticjana7@gmail.com>
2024-06-14 21:01:38 +0200

    admin update potvrdi/otkazi rezervaciju

--- avio-karte-ui/src/stranice/Admin.jsx
+++ avio-karte-ui/src/stranice/Admin.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import Naslov from "../komponente/Naslov";
 import {Chart} from "react-google-charts";
 import axiosZahtev from "../axiosZahtev";
+import {Col, Row, Table} from "react-bootstrap";
 
 const Admin = () => {
     const [poruka, setPoruka] = React.useState("");
@@ -25,6 +26,37 @@ const Admin = () => {
         })
     }, []);
 
+    const [noveRezervacije, setNoveRezervacije] = React.useState([]);
+
+    useEffect(() => {
+        axiosZahtev.get("/nove-rezervacije")
+            .then(res => {
+                setNoveRezervacije(res.data.podaci);
+            }).catch(err => {
+            console.log(err);
+        })
+    }, []);
+
+    const potrvdiRezervaciju = (id) => {
+        axiosZahtev.get("/rezervacije/" + id + "/potvrdi")
+            .then(res => {
+                console.log(res.data);
+                window.location = "/admin";
+            }).catch(err => {
+                console.log(err);
+        })
+    }
+
+    const odbijRezervaciju = (id) => {
+        axiosZahtev.get("/rezervacije/" + id + "/otkazi")
+            .then(res => {
+                console.log(res.data);
+                window.location = "/admin";
+            }).catch(err => {
+                console.log(err);
+        })
+    }
+
     return (
         <>
             <Naslov naslov="Admin panel" poruka={poruka} />
@@ -37,6 +69,43 @@ const Admin = () => {
                     width={"100%"}
                     height={"400px"}
                 />
+                <Row>
+                    <Col>
+                        <Table hover>
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Let ID</th>
+                                <th>Vreme rezervacije</th>
+                                <th>Status</th>
+                                <th>Akcije</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                noveRezervacije && noveRezervacije.map(lett => {
+                                    return (
+                                        <tr key={lett.id}>
+                                            <td>{lett.id}</td>
+                                            <td>{lett.let_id}</td>
+                                            <td>{lett.vreme_rezervacije}</td>
+                                            <td>{lett.status}</td>
+                                            <td>
+                                                <button role="button" onClick={() => {
+                                                    potrvdiRezervaciju(lett.id)
+                                                }} className="btn btn-success m-1">Potvrdi</button>
+                                                <button role="button" onClick={() => {
+                                                    odbijRezervaciju(lett.id)
+                                                }} className="btn btn-danger m-1">Odbij</button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
             </div>
         </>
     );
