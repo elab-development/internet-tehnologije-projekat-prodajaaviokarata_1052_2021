663aa75f10e381f3e7ad5048924428725b3ea73c
andjelacv <andjela.cvijanovic16@gmail.com>
2024-06-25 09:59:22 +0200

    uredjivanje home stranice

--- avio-karte-ui/src/stranice/Home.jsx
+++ avio-karte-ui/src/stranice/Home.jsx
@@ -6,7 +6,7 @@ import avion from '../slike/avion.jpeg';
 const Home = () => {
     return (
         <>
-            <Naslov naslov="Dobrodošli na sajt" poruka="Ovde možete pronaći sve informacije o letovima." />
+            <Naslov naslov="Dobrodošli na sajt" poruka="FlyWithAS - Vaša krila ka svetu. Rezervišite svoj let danas i iskusite vrhunsku uslugu." />
             <div className="glavni mt-3">
                 <Row>
                     <Col md={6}>
@@ -14,9 +14,10 @@ const Home = () => {
                     </Col>
                     <Col md={6}>
                         <p>
-                            Mi smo FlyWithAS, avio kompanija koja se bavi organizacijom letova širom sveta.
-                            Na našem sajtu možete pronaći sve informacije o letovima, kao i mogućnost kupovine karata.
-                            Pored toga, možete se informisati o našoj kompaniji i kontaktirati nas ukoliko imate bilo kakvih pitanja.
+                        Dobrodošli u FlyWithAS, vodeću avio kompaniju specijalizovanu za organizaciju letova širom sveta.
+                        Na našem sajtu možete pronaći sve potrebne informacije o letovima i lako rezervisati karte. 
+                        Takođe, možete saznati više o našoj kompaniji i njenim vrednostima,
+                        kao i kontaktirati nas za bilo kakva pitanja ili dodatnu podršku.
                         </p>
                     </Col>
                 </Row>
